4bb83bb52923c4bb668882895e8904be5af8c668
Viktor Pylypenko <thejustvic@gmail.com>
2023-03-12 23:00:59 +0200

    feat: set prettier and eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import tw from "tailwind-styled-components";
+import tw from 'tailwind-styled-components'
 
 const TwHome = tw.h1`
   text-3xl
   font-bold
   underline
-`;
+`
 
 export default function Home() {
-  return <TwHome>Hello viki!</TwHome>;
+  return <TwHome>Hello viki!</TwHome>
 }
