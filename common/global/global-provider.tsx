a254f301d6db9969d8025fe2fde65949213fbadf
Viktor Pylypenko <thejustvic@gmail.com>
2023-03-24 20:01:25 +0200

    feat: refresh touter on empty session

--- common/global/global-provider.tsx
+++ common/global/global-provider.tsx
@@ -2,7 +2,9 @@
 
 import {GlobalContext, GlobalStore} from '@/common/global/global-store'
 import {Theme} from '@/common/global/types'
-import {useState} from 'react'
+import {useSupabase} from '@/utils/supabase-utils/supabase-provider'
+import {useRouter} from 'next/navigation'
+import {useEffect, useState} from 'react'
 
 interface Props {
   children: React.ReactNode
@@ -10,7 +12,15 @@ interface Props {
 }
 
 export default function GlobalProvider({children}: Props) {
+  const router = useRouter()
+  const {session} = useSupabase()
   const [store] = useState(() => new GlobalStore())
+
+  useEffect(() => {
+    if (!session) {
+      router.refresh()
+    }
+  }, [session])
 
   return (
     <GlobalContext.Provider value={store}>
