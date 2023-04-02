97b9f20b072960ad828336d0b9ae7a2459ec05b4
Viktor Pylypenko <thejustvic@gmail.com>
2023-04-02 12:59:37 +0300

    fixL disable parallax in mobile and safari

--- common/post.tsx
+++ common/post.tsx
@@ -3,6 +3,7 @@ import {IconTrash} from '@tabler/icons-react'
 import Link from 'next/link'
 import {CSSProperties} from 'react'
 import {Button, Card} from 'react-daisyui'
+import {isMobile, isSafari} from 'react-device-detect'
 import Hover from 'react-parallax-tilt'
 import tw from 'tailwind-styled-components'
 
@@ -17,35 +18,52 @@ const TwCard = tw(Card)`
 interface Props {
   post: Post
   remove: () => void
+  disableTranslateZ?: boolean
 }
 
-export const PostContainer = ({post, remove}: Props) => {
+export const PostContainer = (props: Props) => {
+  if (isSafari || isMobile) {
+    return <CardComp {...props} disableTranslateZ />
+  } else {
+    const preserve3D: CSSProperties = {
+      transformStyle: 'preserve-3d'
+    }
+    return (
+      <Hover perspective={800} scale={1.1} style={preserve3D}>
+        <CardComp {...props} />
+      </Hover>
+    )
+  }
+}
+
+const CardComp = ({post, remove, disableTranslateZ}: Props) => {
   const preserve3D: CSSProperties = {
     transformStyle: 'preserve-3d'
   }
-  const translateZ: CSSProperties = {
-    transform: 'translateZ(20px)'
-  }
+
+  const translateZ: CSSProperties = disableTranslateZ
+    ? {}
+    : {
+        transform: 'translateZ(20px)'
+      }
 
   const href = `/?post=${post.id}`
 
   return (
-    <Hover perspective={800} scale={1.1} gyroscope style={preserve3D}>
-      <TwCard bordered compact style={preserve3D}>
-        <Card.Body style={translateZ}>
-          <Card.Title tag="h2" className="flex justify-between">
-            <Link href={href}>{post.text}</Link>
-            <Button color="ghost" shape="circle" onClick={remove}>
-              <IconTrash />
-            </Button>
-          </Card.Title>
-          <Card.Actions className="justify-center">
-            <Button color="primary" fullWidth>
-              Buy Now
-            </Button>
-          </Card.Actions>
-        </Card.Body>
-      </TwCard>
-    </Hover>
+    <TwCard bordered compact style={preserve3D}>
+      <Card.Body style={translateZ}>
+        <Card.Title tag="h2" className="flex justify-between">
+          <Link href={href}>{post.text}</Link>
+          <Button color="ghost" shape="circle" onClick={remove}>
+            <IconTrash />
+          </Button>
+        </Card.Title>
+        <Card.Actions className="justify-center">
+          <Button color="primary" fullWidth>
+            Buy Now
+          </Button>
+        </Card.Actions>
+      </Card.Body>
+    </TwCard>
   )
 }
