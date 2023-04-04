e4dadeb3e34c593ac92683aaec630ca8c50b24f1
Viktor Pylypenko <thejustvic@gmail.com>
2023-04-04 18:47:39 +0300

    feat: use custom scrollbar

--- common/scrollbar/simplebar-scrollbar.tsx
+++ common/scrollbar/simplebar-scrollbar.tsx
@@ -1,10 +1,9 @@
 import {ReactNode} from 'react'
-import SimpleBar from 'simplebar-react'
 
 interface Props {
   children: ReactNode
 }
 
 export const SimpleBarScrollbar = ({children}: Props) => {
-  return <SimpleBar>{children}</SimpleBar>
+  return <div className="scrollbar">{children}</div>
 }
