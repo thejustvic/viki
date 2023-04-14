79694fbed021a4f11c140e33ef83af54503cb98c
Victor Pylypenko <thejustvic@gmail.com>
2023-04-14 05:56:34 +0300

    feat: use-drawer-open-state

--- components/common/wrapper/client-components/drawer-navbar.tsx
+++ components/common/wrapper/client-components/drawer-navbar.tsx
@@ -2,11 +2,11 @@
 
 import {PerfectScrollbar} from '@/components/common/perfect-scrollbar'
 import {useGlobalStore} from '@/components/global/global-store'
+import {useDrawerOpenState} from '@/hooks/use-drawer-open-state'
 import {useGlobalKeyDown} from '@/hooks/use-global-key-down'
-import {useMousePosition} from '@/hooks/use-mouse-position'
 import {headerHeight} from '@/utils/const'
 import {observer} from 'mobx-react-lite'
-import {ReactNode, useEffect} from 'react'
+import {ReactNode} from 'react'
 import {Drawer} from 'react-daisyui'
 import {isMobile} from 'react-device-detect'
 import {DrawerMenu} from '../../drawer-menu'
@@ -18,24 +18,15 @@ interface Props {
 
 export const DrawerNavbar = observer(({children}: Props) => {
   const [state, store] = useGlobalStore()
-  const {x} = useMousePosition()
 
   const closeDrawer = () => {
     store.setDrawerClosed()
   }
 
+  useDrawerOpenState()
   useGlobalKeyDown({
     escape: () => state.drawerOpen && closeDrawer()
   })
-
-  useEffect(() => {
-    if (!state.drawerOpen && !state.drawerOpenByHover && Number(x) < 10) {
-      store.setDrawerOpen(true)
-    }
-    if (state.drawerOpen && state.drawerOpenByHover && Number(x) > 400) {
-      store.setDrawerClosed(false)
-    }
-  }, [x])
 
   return (
     <Drawer
---
+++ hooks/use-drawer-open-state.ts
@@ -0,0 +1,17 @@
+import {useGlobalStore} from '@/components/global/global-store'
+import {useEffect} from 'react'
+import {useMousePosition} from './use-mouse-position'
+
+export const useDrawerOpenState = (): void => {
+  const {x} = useMousePosition()
+  const [state, store] = useGlobalStore()
+
+  useEffect(() => {
+    if (!state.drawerOpen && !state.drawerOpenByHover && Number(x) < 10) {
+      store.setDrawerOpen(true)
+    }
+    if (state.drawerOpen && state.drawerOpenByHover && Number(x) > 400) {
+      store.setDrawerClosed(false)
+    }
+  }, [x])
+}
