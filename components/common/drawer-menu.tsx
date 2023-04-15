9c683acd03df2f33622f8bd54345cfa094dead09
Viktor Pylypenko <thejustvic@gmail.com>
2023-04-15 19:20:14 +0300

    feat: open drawer with margins and rounding on hover

--- components/common/drawer-menu.tsx
+++ components/common/drawer-menu.tsx
@@ -4,23 +4,24 @@ import {Button, CodeMockup, Divider, Menu} from 'react-daisyui'
 import tw from 'tailwind-styled-components'
 import {useGlobalStore} from '../global/global-store'
 
-const TwMenu = tw(Menu)`
+const TwMenu = tw(Menu)<{$margin: boolean}>`
   bg-base-100 
   text-base-content 
   overflow-y-auto 
   w-80 
   p-2
+  ${p => (p.$margin ? 'my-8 ml-2 rounded-lg' : '')}
 `
 
 export const DrawerMenu = observer(() => {
-  const [, store] = useGlobalStore()
+  const [state, store] = useGlobalStore()
 
   const closeDrawer = () => {
     store.setDrawerClosed()
   }
 
   return (
-    <TwMenu vertical>
+    <TwMenu vertical $margin={state.drawerOpenByHover}>
       <Link href="/">
         <Button color="ghost" className="w-full text-xl" onClick={closeDrawer}>
           viki
