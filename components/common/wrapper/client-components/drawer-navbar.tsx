a52b05dce10b601e337dcfaf33bb64f18611e7d9
Viktor Pylypenko <thejustvic@gmail.com>
2023-04-22 11:23:08 +0300

    refactor: remove vh and svh height from drawer

--- components/common/wrapper/client-components/drawer-navbar.tsx
+++ components/common/wrapper/client-components/drawer-navbar.tsx
@@ -5,7 +5,7 @@ import {useGlobalStore} from '@/components/global/global-store'
 import {useDrawerOpenState} from '@/hooks/use-drawer-open-state'
 import {headerHeight} from '@/utils/const'
 import {observer} from 'mobx-react-lite'
-import {ReactNode, useEffect, useRef} from 'react'
+import {ReactNode} from 'react'
 import {Drawer} from 'react-daisyui'
 import {isMobile} from 'react-device-detect'
 import {DrawerMenu} from '../../drawer-menu'
@@ -17,17 +17,8 @@ interface Props {
 
 export const DrawerNavbar = observer(({children}: Props) => {
   const [state, store] = useGlobalStore()
-  const ref = useRef<HTMLDivElement>(null)
 
   useDrawerOpenState()
-
-  useEffect(() => {
-    const height = `
-      height: calc(100vh - ${headerHeight}); /* fallback for Firefox, IE and etc. */
-      height: calc(100svh - ${headerHeight});
-    `
-    ref.current?.setAttribute('style', height)
-  }, [])
 
   const closeDrawer = () => {
     store.setDrawerClosed()
@@ -43,7 +34,7 @@ export const DrawerNavbar = observer(({children}: Props) => {
       side={<DrawerMenu />}
     >
       <Navbar />
-      <div ref={ref} style={{height: `calc(100% - ${headerHeight})`}}>
+      <div style={{height: `calc(100% - ${headerHeight})`}}>
         <PerfectScrollbar>{children}</PerfectScrollbar>
       </div>
     </Drawer>
