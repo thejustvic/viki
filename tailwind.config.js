a643db99d03b1d9741fb8a0e70457776eb93d41a
Viktor Pylypenko <thejustvic@gmail.com>
2023-04-22 19:56:39 +0300

    feat: add classes to tailwind via custom plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const MyClass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+
 module.exports = {
     content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -20,5 +38,5 @@ module.exports = {
       }
     },
   },
-  plugins: [require('daisyui')]
+  plugins: [require('daisyui'), MyClass]
 }
