65db51b15f48c3cd07e6e80f145d9ad03b6de9e0
Viktor Pylypenko <thejustvic@gmail.com>
2023-04-25 17:41:38 +0300

    refactor: email login

---
+++ components/login/email-login/login-card.tsx
@@ -0,0 +1,93 @@
+import {useSupabase} from '@/utils/supabase-utils/supabase-provider'
+import {observer} from 'mobx-react-lite'
+import {Card} from 'react-daisyui'
+import tw from 'tailwind-styled-components'
+import {useEmailLoginStore} from './email-login-store'
+import {LoginForm} from './login-form'
+
+const TwLoginCard = tw.div`
+  w-[300px] 
+  h-[358px] 
+  perspective
+`
+
+const TwLoginCardInner = tw.div<{$rotate: boolean}>`
+  relative 
+  w-full 
+  h-full 
+  duration-700 
+  preserve-3d
+  ${p => (p.$rotate ? 'my-rotate-y-180' : '')}
+`
+
+const TwCardLogin = tw.div`
+  absolute 
+  w-full 
+  h-full 
+  backface-hidden
+`
+
+const TwCardRegister = tw.div`
+  absolute 
+  w-full 
+  h-full 
+  my-rotate-y-180 
+  backface-hidden
+`
+
+const TwCard = tw(Card)`
+  flex-shrink-0 
+  w-full 
+  max-w-sm 
+  shadow-2xl 
+  bg-base-100
+`
+
+export const LoginCard = observer(() => {
+  const [state] = useEmailLoginStore()
+
+  return (
+    <TwLoginCard>
+      <TwLoginCardInner $rotate={state.view === 'register'}>
+        <TwCardLogin>
+          <TwCard>
+            <Login />
+          </TwCard>
+        </TwCardLogin>
+        <TwCardRegister>
+          <TwCard>
+            <Register />
+          </TwCard>
+        </TwCardRegister>
+      </TwLoginCardInner>
+    </TwLoginCard>
+  )
+})
+
+const Login = observer(() => {
+  const [, store] = useEmailLoginStore()
+  const {supabase} = useSupabase()
+
+  return (
+    <LoginForm
+      title="Login"
+      linkTitle="Don't have an account?"
+      handleLink={store.setRegisterView}
+      handleAuth={data => supabase.auth.signInWithPassword(data)}
+    />
+  )
+})
+
+const Register = observer(() => {
+  const [, store] = useEmailLoginStore()
+  const {supabase} = useSupabase()
+
+  return (
+    <LoginForm
+      title="Register"
+      linkTitle="Already have an account?"
+      handleLink={store.setLoginView}
+      handleAuth={data => supabase.auth.signUp(data)}
+    />
+  )
+})
---
+++ components/login/email-login/login-form.tsx
@@ -0,0 +1,104 @@
+import {AuthResponse} from '@supabase/supabase-js'
+import {Button, Card, Form, Input, Link} from 'react-daisyui'
+import {SubmitHandler, useForm} from 'react-hook-form'
+import tw from 'tailwind-styled-components'
+
+const TwTitle = tw(Card.Title)`
+  flex 
+  justify-center 
+  pt-4
+`
+
+const TwBody = tw(Card.Body)`
+  pt-0 
+  pb-4
+`
+
+const TwSubmit = tw(Button)`
+  mt-6
+`
+
+const TwError = tw.p`
+  w-full 
+  mt-2 
+  text-xs 
+  text-center 
+  text-error
+`
+
+const TwLink = tw(Link)`
+  flex 
+  justify-center
+`
+
+interface LoginFormProps {
+  title: 'Login' | 'Register'
+  linkTitle: 'Already have an account?' | "Don't have an account?"
+  handleLink: () => void
+  handleAuth: (data: FormValues) => Promise<AuthResponse>
+}
+
+interface FormValues {
+  email: string
+  password: string
+}
+
+export const LoginForm = ({
+  handleLink,
+  handleAuth,
+  title,
+  linkTitle
+}: LoginFormProps) => {
+  const {
+    register,
+    setError,
+    handleSubmit,
+    formState: {errors}
+  } = useForm<FormValues>()
+
+  const onSubmit: SubmitHandler<FormValues> = async data => {
+    const {error} = await handleAuth(data)
+    if (error) {
+      setError(
+        'email',
+        {type: 'focus', message: error.message},
+        {shouldFocus: true}
+      )
+    }
+  }
+
+  return (
+    <>
+      <TwTitle>{title}</TwTitle>
+      <TwBody>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Label title="Email" />
+          <Input
+            type="text"
+            placeholder="email"
+            {...register('email', {
+              required: true,
+              pattern: /^\S+@\S+$/i
+            })}
+          />
+          <Form.Label title="Password" />
+          <Input
+            type="password"
+            placeholder="password"
+            {...register('password', {
+              required: true,
+              minLength: {
+                value: 6,
+                message: 'Password should be at least 6 characters'
+              }
+            })}
+          />
+          <TwSubmit type="submit">Submit</TwSubmit>
+          {errors.email && <TwError>{errors.email.message}</TwError>}
+          {errors.password && <TwError>{errors.password.message}</TwError>}
+        </Form>
+        <TwLink onClick={handleLink}>{linkTitle}</TwLink>
+      </TwBody>
+    </>
+  )
+}
