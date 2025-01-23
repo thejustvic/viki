92d7596e9bbb24414c249a29041d7b8e11337d9f
Viktor Pylypenko <thejustvic@gmail.com>
2025-01-23 02:47:33 +0200

    feat: move drag svg to separate function

--- components/common/wrapper/client-components/drawer-navbar.tsx
+++ components/common/wrapper/client-components/drawer-navbar.tsx
@@ -176,25 +176,7 @@ const Drag = observer(() => {
 
   return (
     <TwDragWrap onMouseDown={handleMouseDown} $show={mouseDown.value}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        className={`h-2.5 w-2.5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
-      >
-        <circle cx="9" cy="12" r="1"></circle>
-        <circle cx="9" cy="5" r="1"></circle>
-        <circle cx="9" cy="19" r="1"></circle>
-        <circle cx="15" cy="12" r="1"></circle>
-        <circle cx="15" cy="5" r="1"></circle>
-        <circle cx="15" cy="19" r="1"></circle>
-      </svg>
+      <DragSvg />
     </TwDragWrap>
   )
 })
@@ -217,3 +199,27 @@ const TwDragWrap = tw.div<{$show: boolean}>`
   duration-200
   ${p => p.$show && 'opacity-100'}
 `
+
+const DragSvg = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={`h-2.5 w-2.5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
+    >
+      <circle cx="9" cy="12" r="1"></circle>
+      <circle cx="9" cy="5" r="1"></circle>
+      <circle cx="9" cy="19" r="1"></circle>
+      <circle cx="15" cy="12" r="1"></circle>
+      <circle cx="15" cy="5" r="1"></circle>
+      <circle cx="15" cy="19" r="1"></circle>
+    </svg>
+  )
+}
