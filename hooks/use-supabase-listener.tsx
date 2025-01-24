6c66fb93d9b946e41a8c225eaf605c8a34187f54
Viktor Pylypenko <thejustvic@gmail.com>
2025-01-24 18:50:19 +0200

    feat: remove hash from the URL after signin

--- hooks/use-supabase-listener.tsx
+++ hooks/use-supabase-listener.tsx
@@ -26,6 +26,18 @@ export const useSupabaseListener = (
       if (session?.access_token !== mySession?.access_token) {
         router.refresh()
       }
+      // Check if there's a hash fragment in the URL
+      if (window.location.hash) {
+        const hashParams = new URLSearchParams(
+          window.location.hash.substring(1)
+        )
+        const accessToken = hashParams.get('access_token')
+
+        if (accessToken) {
+          // Remove the disgusting hash from the URL without reloading the page
+          window.history.replaceState(null, '', window.location.pathname)
+        }
+      }
     })
 
     return () => {
