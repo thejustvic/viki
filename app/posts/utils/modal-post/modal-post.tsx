370ddff3e255a6e4570be5f9654d7a49ac22915a
Viktor Pylypenko <thejustvic@gmail.com>
2025-02-01 01:05:15 +0200

    fix: textarea dummy component value state without on change

--- app/posts/utils/modal-post/modal-post.tsx
+++ app/posts/utils/modal-post/modal-post.tsx
@@ -97,7 +97,7 @@ const TextData = observer(() => {
   if (!modalState.post.data) {
     return (
       <div className="relative w-full">
-        <Textarea size="md" value={''} className="w-full" />
+        <Textarea size="md" value={''} className="w-full" onChange={() => {}} />
         <TwLoading className="absolute transform -translate-x-1/2 -translate-y-2/3 top-1/2 left-1/2" />
       </div>
     )
