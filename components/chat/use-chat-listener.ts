ce37ec12b6a452cb0027a73cae81668da17e6b54
Viktor Pylypenko <thejustvic@gmail.com>
2025-03-14 21:23:28 +0200

    refactor: use-chat-listener

--- components/chat/use-chat-listener.ts
+++ components/chat/use-chat-listener.ts
@@ -5,21 +5,20 @@ import {Tables} from '@/utils/database.types'
 import {SupabaseContext} from '@/utils/supabase-utils/supabase-provider'
 import {Util} from '@/utils/util'
 import type {PostgrestBuilder} from '@supabase/postgrest-js'
-import {useEffect} from 'react'
+import {useEffect, useMemo} from 'react'
 import {ChatStore} from './chat-store'
 import {Message} from './types'
 
 const getMessages = (
   postId: Post['id'],
   supabase: SupabaseContext['supabase']
-): PostgrestBuilder<Tables<'messages'>[]> => {
-  return supabase
+): PostgrestBuilder<Tables<'messages'>[]> =>
+  supabase
     .from('messages')
     .select()
     .eq('post_id', postId)
     .order('created_at')
     .throwOnError()
-}
 
 export const useChatListener = (
   user: SupabaseContext['user'],
@@ -28,21 +27,23 @@ export const useChatListener = (
 ): void => {
   const postId = getSearchPost()
 
-  const {data, loading, error} = useSupabaseFetch(
-    postId ? () => getMessages(postId, supabase) : null,
-    [postId]
+  // Memoize `getMessages` to prevent unnecessary re-renders
+  const fetchMessages = useMemo(
+    () => (postId ? () => getMessages(postId, supabase) : null),
+    [postId, supabase]
   )
 
+  // Fetch messages using custom hook
+  const {data, loading, error} = useSupabaseFetch(fetchMessages, [postId])
+
   useEffect(() => {
-    const messages = Util.explicitlyCastFromJsonToReactions(data)
-    if (messages) {
-      store.setChat({
-        loading,
-        data: messages,
-        error
-      })
+    if (!data) {
+      return // Avoid updating store with null data
     }
-  }, [data, loading, error])
+
+    const messages = Util.explicitlyCastFromJsonToReactions(data)
+    store.setChat({loading, data: messages, error})
+  }, [data, loading, error, store])
 
   useEffect(() => {
     const channel = supabase
