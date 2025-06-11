a402715ddbf3b4456613b2f5debd5606f6ec0bc1
Viktor Pylypenko <thejustvic@gmail.com>
2025-06-11 19:04:44 +0300

    feat: redirect to login without session

--- app/cards/layout.tsx
+++ app/cards/layout.tsx
@@ -1,3 +1,26 @@
-export default function CardsLayout({children}: {children: React.ReactNode}) {
-  return <section>{children}</section>
+import {Load} from '@/components/common/load'
+import {getServerSession} from '@/utils/supabase-utils/get-server-session'
+import {headers} from 'next/headers'
+import {redirect} from 'next/navigation'
+import {Suspense} from 'react'
+
+export default async function CardsLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const heads = await headers()
+  const pathname = heads.get('x-invoke-path') ?? ''
+
+  const session = await getServerSession()
+
+  if (!session && pathname !== '/login') {
+    redirect('/login')
+  }
+
+  return (
+    <Suspense fallback={<Load center />}>
+      <section>{children}</section>
+    </Suspense>
+  )
 }
