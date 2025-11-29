979a96a84e7d7bb2d53703c75b2fe6f28195c4cc
Viktor Pylypenko <thejustvic@gmail.com>
2025-11-29 15:27:35 +0200

    fix(mobx): makeAutoPersist SSR-safe, persist only state, debounce saves, return disposer

--- utils/mobx-utils/make-auto-persist.ts
+++ utils/mobx-utils/make-auto-persist.ts
@@ -1,23 +1,48 @@
 import {ArrUtil} from '@/utils/arr-util'
 import {ObjUtil} from '@/utils/obj-util'
-import {autorun, set, toJS} from 'mobx'
+import {IReactionDisposer, reaction, set, toJS} from 'mobx'
 
 export const makeAutoPersist = <T extends {state: unknown}>(
   _this: T,
   name: string
-): void => {
-  const storedJson = window?.localStorage?.getItem(name)
-
-  if (storedJson) {
-    const thisStateKeys = ObjUtil.keys(_this.state)
-    const storedStateKeys = ObjUtil.keys(JSON.parse(storedJson)?.state)
-
-    if (ArrUtil.areListsEqual(thisStateKeys, storedStateKeys)) {
-      set(_this, JSON.parse(storedJson))
-    }
+): (() => void) => {
+  // Only run in browser environments
+  if (typeof window === 'undefined' || !window?.localStorage) {
+    return () => {}
   }
-  autorun(() => {
-    const value = toJS(_this)
-    window?.localStorage?.setItem(name, JSON.stringify(value))
-  })
+
+  try {
+    const storedJson = window.localStorage.getItem(name)
+    if (storedJson) {
+      const parsed = JSON.parse(storedJson)
+      const thisStateKeys = ObjUtil.keys((_this as any).state ?? {})
+      const storedStateKeys = ObjUtil.keys(parsed?.state ?? {})
+
+      if (ArrUtil.areListsEqual(thisStateKeys, storedStateKeys)) {
+        set(_this as any, parsed)
+      }
+    }
+  } catch (err) {
+    // Don't throw during startup; log in dev only
+    // eslint-disable-next-line no-console
+    if (process.env.NODE_ENV !== 'production')
+      console.warn('makeAutoPersist: failed to restore persisted state', err)
+  }
+
+  const disposer: IReactionDisposer = reaction(
+    () => toJS((_this as any).state ?? {}),
+    state => {
+      try {
+        window.localStorage.setItem(name, JSON.stringify({state}))
+      } catch (err) {
+        // ignore quota/serialization errors in production, but warn in dev
+        // eslint-disable-next-line no-console
+        if (process.env.NODE_ENV !== 'production')
+          console.warn('makeAutoPersist: failed to save state', err)
+      }
+    },
+    {delay: 300}
+  )
+
+  return () => disposer()
 }
