1e133d3522c7e4d15c0aa90d59299687df9784b2
Viktor Pylypenko <thejustvic@gmail.com>
2025-12-09 18:15:49 +0200

    feat: add scene canvas with rapier physics

---
+++ components/cards/visual/ui/base-scene.tsx
@@ -0,0 +1,29 @@
+import {Loader, PointerLockControls} from '@react-three/drei'
+import {Canvas} from '@react-three/fiber'
+import {Physics} from '@react-three/rapier'
+import {Floor} from '../components/floor'
+import {Lights} from '../components/lights'
+
+interface BasicSceneProps {
+  children: React.ReactNode
+}
+
+const BasicScene = ({children}: BasicSceneProps) => {
+  return (
+    <div style={{height: 'calc(100vh - 41px)', width: '100vw'}}>
+      <Canvas shadows camera={{fov: 50}}>
+        <Lights />
+
+        <Physics gravity={[0, -9.8, 0]}>
+          {children}
+          <Floor color="white" />
+        </Physics>
+
+        <PointerLockControls />
+      </Canvas>
+      <Loader />
+    </div>
+  )
+}
+
+export default BasicScene
