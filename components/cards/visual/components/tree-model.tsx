c9b8b23a988e140586bfbdba3de8a024a544b1eb
Viktor Pylypenko <thejustvic@gmail.com>
2025-12-10 16:53:55 +0200

    feat: add shadow for tree model

--- components/cards/visual/components/tree-model.tsx
+++ components/cards/visual/components/tree-model.tsx
@@ -10,11 +10,22 @@ interface ThreeModelProps {
 export const TreeModel = ({scale, position}: ThreeModelProps) => {
   const {scene} = useGLTF('/christmas_tree.glb')
 
+  scene.traverse(node => {
+    node.castShadow = true
+    node.receiveShadow = true
+  })
+
   return (
     <Suspense fallback={null}>
       {/* "hull" for complex shapes, "cuboid" for boxes, etc. */}
       <RigidBody colliders="hull" type="fixed">
-        <primitive object={scene} scale={scale} position={position} />
+        <primitive
+          object={scene}
+          scale={scale}
+          position={position}
+          castShadow
+          receiveShadow
+        />
       </RigidBody>
     </Suspense>
   )
