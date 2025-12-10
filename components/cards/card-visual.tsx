3d8633ec2b6385cc809f3e8627600c72826e47e7
Viktor Pylypenko <thejustvic@gmail.com>
2025-12-10 16:47:04 +0200

    feat: move boxes to the right to reveal a view of the christmas tree

--- components/cards/card-visual.tsx
+++ components/cards/card-visual.tsx
@@ -9,9 +9,9 @@ export default function CardVisual() {
     <BaseScene>
       <TreeModel position={[0, -1, -10]} scale={0.3} />
 
-      <BaseBox position={[-2, 1, -5]} args={[1, 1, 1]} color="green" />
-      <BaseBox position={[0, 1, -5]} args={[1, 2, 1]} color="red" />
-      <BaseBox position={[2, 1, -5]} args={[1, 3, 1]} color="orange" />
+      <BaseBox position={[5, 1, -8]} args={[1, 1, 1]} color="green" />
+      <BaseBox position={[5, 1, -6]} args={[1, 2, 1]} color="red" />
+      <BaseBox position={[5, 1, -4]} args={[1, 3, 1]} color="orange" />
 
       <BaseCharacter position={[0, 1, 0]} args={[0.5]} />
 
