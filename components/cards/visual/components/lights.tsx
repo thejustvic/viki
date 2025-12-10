1f27efb819b22936e565a480ade9512860252379
Viktor Pylypenko <thejustvic@gmail.com>
2025-12-10 09:34:40 +0200

    feat: cast shadows to the floor by directionalLight

--- components/cards/visual/components/lights.tsx
+++ components/cards/visual/components/lights.tsx
@@ -1,6 +1,17 @@
 export const Lights = () => (
   <>
     <ambientLight intensity={1} />
-    <spotLight penumbra={0.5} position={[10, 10, 5]} castShadow />
+    <directionalLight
+      position={[5, 10, 5]}
+      intensity={0.8}
+      castShadow
+      shadow-mapSize={[2048, 2048]} // increase shadow map resolution
+      shadow-camera-near={0.1}
+      shadow-camera-far={50}
+      shadow-camera-left={-10}
+      shadow-camera-right={10}
+      shadow-camera-top={10}
+      shadow-camera-bottom={-10}
+    />
   </>
 )
