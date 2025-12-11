8a0402609fb697b0baa7bc0991d56737acc1537b
Viktor Pylypenko <thejustvic@gmail.com>
2025-12-11 14:54:23 +0200

    feat: add util to create text texture to use it on sphere

---
+++ components/cards/visual/utils/create-text-texture.ts
@@ -0,0 +1,37 @@
+import {CanvasTexture} from 'three'
+
+export const createTextTexture = (
+  text: string,
+  color: string = '#ffffff',
+  bgColor: string = '#ff0000'
+): CanvasTexture => {
+  const canvas = document.createElement('canvas')
+  const context = canvas.getContext('2d')
+
+  if (!context) {
+    throw new Error('Could not get canvas context')
+  }
+
+  const size = 512
+  canvas.width = size
+  canvas.height = size
+
+  // Draw background
+  context.fillStyle = bgColor
+  context.fillRect(0, 0, size, size)
+
+  // Draw the text
+  context.fillStyle = color
+  context.font = '16px Arial'
+  context.textAlign = 'center'
+  context.textBaseline = 'middle'
+
+  context.fillText(text, size / 4, size / 2)
+
+  // console.log(canvas.toDataURL()) // Uncomment this to see the generated image in your browser console
+
+  const texture = new CanvasTexture(canvas)
+  texture.needsUpdate = true
+
+  return texture
+}
