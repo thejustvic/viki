8b1c2a661aa2cfd1315f9b7f0a446c6af41905eb
Viktor Pylypenko <thejustvic@gmail.com>
2025-12-11 14:59:05 +0200

    feat: update scene with sphere on tree

--- components/cards/card-visual.tsx
+++ components/cards/card-visual.tsx
@@ -3,11 +3,14 @@ import {TreeModel} from './visual/components/tree-model'
 import BaseBox from './visual/ui/base-box'
 import BaseCharacter from './visual/ui/base-character'
 import BaseScene from './visual/ui/base-scene'
+import {BaseSphere} from './visual/ui/base-sphere'
 
 export default function CardVisual() {
   return (
     <BaseScene>
       <TreeModel position={[0, -1, -10]} scale={0.3} />
+
+      <BaseSphere position={[0, 2, -8]} text="Merry Xmas" />
 
       <BaseBox position={[5, 1, -8]} args={[1, 1, 1]} color="green" />
       <BaseBox position={[5, 1, -6]} args={[1, 2, 1]} color="red" />
