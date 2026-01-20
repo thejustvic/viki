35c8d08b78c953cb6446b19d362f3a0f075aa28f
Viktor Pylypenko <thejustvic@gmail.com>
2026-01-20 10:21:44 +0200

    feat: implement first-person controls with button toggle in BasicScene component

--- components/cards/visual/ui/base-scene.tsx
+++ components/cards/visual/ui/base-scene.tsx
@@ -1,5 +1,6 @@
+import {Button} from '@/components/daisyui/button'
+import {useBoolean} from '@/hooks/use-boolean'
 import {Environment, Loader, PointerLockControls} from '@react-three/drei'
-
 import {Canvas} from '@react-three/fiber'
 import {Physics} from '@react-three/rapier'
 import tw from 'tailwind-styled-components'
@@ -26,8 +27,18 @@ interface BasicSceneProps {
 }
 
 const BasicScene = ({children}: BasicSceneProps) => {
+  const isLocked = useBoolean(false)
   return (
     <div style={{height: 'calc(100vh - 41px)', width: '100vw'}}>
+      {!isLocked.value ? (
+        <Button className="m-2 absolute z-10" id="enter-btn">
+          Enter First Person With Movement by WASD keys
+        </Button>
+      ) : (
+        <div className="m-2 text-pretty absolute z-10 text-neutral-400">
+          press ESC to Exit First Person
+        </div>
+      )}
       <Canvas shadows camera={{fov: 50, position: [-0.2, -1, 5]}}>
         <Lights />
 
@@ -40,7 +51,11 @@ const BasicScene = ({children}: BasicSceneProps) => {
         {/* Environment map for realistic reflections */}
         <Environment preset="sunset" />
 
-        <PointerLockControls />
+        <PointerLockControls
+          selector="#enter-btn"
+          onLock={isLocked.turnOn}
+          onUnlock={isLocked.turnOff}
+        />
       </Canvas>
       <Loader />
       <TwDot />
