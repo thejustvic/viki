b0a25365044355204bbb64881a2b49dd3808af31
Viktor Pylypenko <thejustvic@gmail.com>
2026-02-21 13:35:45 +0200

    refactor: remove mobile and tablet specific rendering from SimpleScrollbar component

--- components/common/simple-scrollbar.tsx
+++ components/common/simple-scrollbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import {HTMLProps, ReactNode} from 'react'
-import {isMobile, isTablet} from 'react-device-detect'
 import SimpleBarReact from 'simplebar-react'
 
 interface Props {
@@ -11,16 +10,6 @@ interface Props {
 }
 
 export const SimpleScrollbar = ({children, className, style}: Props) => {
-  if (isMobile || isTablet) {
-    return (
-      <div
-        style={{overflowY: 'auto', height: '100%', minHeight: 0, ...style}}
-        className={className}
-      >
-        {children}
-      </div>
-    )
-  }
   return (
     <SimpleBarReact
       className={className}
