282cc58797a3168434684c28d4498b2570cb8022
Viktor Pylypenko <thejustvic@gmail.com>
2026-02-28 18:25:20 +0200

    refactor: remove boxes from scene

--- components/cards/card-visual.tsx
+++ components/cards/card-visual.tsx
@@ -3,7 +3,6 @@ import {Sky} from '@react-three/drei'
 import {Checkbox} from '../checklist/types'
 import {CardInfoStore} from './card-info/card-info-store'
 import {CardVisualType} from './types'
-import BaseBox from './visual/ui/base-box'
 import {BaseCharacter} from './visual/ui/base-character'
 import BaseScene from './visual/ui/base-scene'
 import {ConeWithSpheres} from './visual/ui/cone'
@@ -29,13 +28,8 @@ export default function CardVisual({
         <ConeWithSpheres checklist={checklist} cardInfoState={cardInfoState} />
       )}
 
-      <BaseBox position={[5, 1, -8]} args={[1, 1, 1]} color="green" />
-      <BaseBox position={[5, 1, -6]} args={[1, 2, 1]} color="red" />
-      <BaseBox position={[5, 1, -4]} args={[1, 3, 1]} color="orange" />
-
-      <BaseCharacter position={[0, 1, -4]} args={[0.5]} isLocked={isLocked} />
-
       <Sky sunPosition={[5, 10, 5]} turbidity={0.25} />
+      <BaseCharacter position={[0, 0, 0]} args={[0.5]} isLocked={isLocked} />
     </BaseScene>
   )
 }
