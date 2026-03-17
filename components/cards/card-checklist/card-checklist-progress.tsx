b10be1c5005390a69e5f3c0f46587696cb8e3321
Viktor Pylypenko <thejustvic@gmail.com>
2026-03-17 11:08:14 +0200

    refactor: card checklist progress

--- components/cards/card-checklist/card-checklist-progress.tsx
+++ components/cards/card-checklist/card-checklist-progress.tsx
@@ -1,7 +1,39 @@
+import tw from '@/components/common/tw-styled-components'
 import {observer} from 'mobx-react-lite'
 import {useEffect} from 'react'
-import {twJoin} from 'tailwind-merge'
 import {useCardChecklistStore} from './card-checklist-store'
+
+const TwWrapper = tw.div`
+  flex
+  flex-1
+  gap-2
+  items-center
+`
+
+const TwProgressWrapper = tw.div`
+  relative
+  flex-1
+  h-1.5
+  bg-info-content
+  rounded-sm
+`
+
+interface ITwProgress {
+  $isFull: boolean
+}
+const TwProgress = tw.div<ITwProgress>`
+  ${({$isFull}) => ($isFull ? 'bg-success' : 'bg-info')}
+  h-full
+  rounded-sm
+  min-w-2
+  transition-[width]
+  duration-300
+`
+
+const TwProgressText = tw.div`
+  text-xs
+  text-center
+`
 
 export const CardChecklistProgress = observer(({id}: {id: string}) => {
   const [state, store] = useCardChecklistStore()
@@ -14,20 +46,18 @@ export const CardChecklistProgress = observer(({id}: {id: string}) => {
     }
   }, [state.checklists.data])
 
+  const progress = state.progress.get(id)
+  const progressText = state.progressText.get(id)
+
   return (
-    <div className="flex flex-1 gap-2 items-center">
-      <div className="relative flex-1 h-1.5 bg-info-content rounded-sm">
-        <div
-          className={twJoin(
-            'h-full rounded-sm min-w-2 transition-[width] duration-300',
-            state.progress.get(id) === 100 ? 'bg-success' : 'bg-info'
-          )}
-          style={{width: (state.progress.get(id) ?? 0) + '%'}}
+    <TwWrapper>
+      <TwProgressWrapper>
+        <TwProgress
+          $isFull={progress === 100}
+          style={{width: (progress ?? 0) + '%'}}
         />
-      </div>
-      <div className={twJoin('text-xs text-center')}>
-        {state.progressText.get(id) ?? '0/0'}
-      </div>
-    </div>
+      </TwProgressWrapper>
+      <TwProgressText>{progressText ?? '0/0'}</TwProgressText>
+    </TwWrapper>
   )
 })
