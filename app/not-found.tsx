34b989fc3e7a060a6c4b832557b9431c3116829c
Viktor Pylypenko <thejustvic@gmail.com>
2026-03-17 21:57:26 +0200

    reactor: not found page with motion

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,52 @@
 'use client'
 
+import tw from '@/components/common/tw-styled-components'
 import {Button} from '@/components/daisyui/button'
 import {motion} from 'framer-motion'
 import Link from 'next/link'
 
+const TwNotFound = tw.div`
+  flex
+  flex-col
+  items-center
+  justify-center
+  h-screen
+  text-center
+`
+
+const TwMotionH1 = tw(motion.h1)`
+  text-6xl
+  font-bold
+`
+
+const TwMotionP = tw(motion.p)`
+  mt-4
+  text-xl
+  text-gray-600
+`
+
+const TwMotionDiv = tw(motion.div)`
+  mt-6
+`
+
 export default function NotFoundPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
-      <motion.h1
-        className="text-6xl font-bold"
+    <TwNotFound>
+      <TwMotionH1
         initial={{opacity: 0, y: -20}}
         animate={{opacity: 1, y: 0}}
         transition={{duration: 0.5}}
       >
         404
-      </motion.h1>
-      <motion.p
-        className="mt-4 text-xl text-gray-600"
+      </TwMotionH1>
+      <TwMotionP
         initial={{opacity: 0}}
         animate={{opacity: 1}}
         transition={{delay: 0.3}}
       >
         Oops! The page you are looking for does not exist.
-      </motion.p>
-      <motion.div
-        className="mt-6"
+      </TwMotionP>
+      <TwMotionDiv
         initial={{opacity: 0}}
         animate={{opacity: 1}}
         transition={{delay: 0.6}}
@@ -32,7 +54,7 @@ export default function NotFoundPage() {
         <Button soft color="info">
           <Link href="/cards">Go Home</Link>
         </Button>
-      </motion.div>
-    </div>
+      </TwMotionDiv>
+    </TwNotFound>
   )
 }
