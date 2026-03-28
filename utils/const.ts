2a79d5386b61e0478621b819a70eb0eef1e0d761
Viktor Pylypenko <thejustvic@gmail.com>
2026-03-28 20:26:47 +0200

    feat: increase minimum drawer width

--- utils/const.ts
+++ utils/const.ts
@@ -3,6 +3,6 @@ export const cardHeight = 142
 
 export const minNavbarWidth = 500
 
-export const minDrawerWidth = 320
+export const minDrawerWidth = 350
 export const maxWidthLeftDrawer = 2500
 export const maxWidthRightDrawer = 2500
